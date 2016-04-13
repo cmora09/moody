769b6f9c5271facebbf16d55d3adceb145fa82c3
Christopher Mora <cmora09@gmail.com>
2016-04-13 12:38:06 -0400

    Spotify sign in works

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,11 +2,10 @@ class CallbacksController < ApplicationController
   def spotify
   	# this is grabbing the .from_omniauth method from our User model where it's self.from_omniauth
   	@user = User.from_omniauth(request.env["omniauth.auth"])
-  	
   	# checks to see if user exists in database.
   	if @user.persisted?
   		#if valid user is found itll sign in and redirect user
-  		sign_in_and_redirect @user, :event => :authentication
+  		sign_in_and_redirect '/', :event => :authentication
   		set_flash_message(:notice, :success, :kind => "Spotify") if is_navigational_format?
   	else
   		#request.env["omniauth.auth"] grants info as a hash from spotify
@@ -14,6 +13,7 @@ class CallbacksController < ApplicationController
   		redirect_to new_user_registration_url
   	end
   end
+  
   def failure
   	redirect_to root_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,8 @@ class User < ActiveRecord::Base
   #this method checks to see if provider and uid strings exist in the database. if they don't itll create them with a random password
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  		user.provider = auth.provider
-  		user.uid = auth.uid
   		user.email = auth.info.email
   		user.password = Devise.friendly_token[0,20]
-  	end
-  end
-
-  #copies info from spotify to createa a new user with session
-  def self.new_with_session(params,session)
-  	super.tap do |user|
-  		if data = session["devise.spotify_data"] && session["devise.spotify_data"]["extra"]["raw_info"]
-  			user.email = data["email"]	if user.email.blank?
-  		end
   	end
   end
 
